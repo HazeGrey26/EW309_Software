fece895abcd21c5aceb81c7fda059729a5371ff7
HazeGrey26 <147719792+HazeGrey26@users.noreply.github.com>
2025-04-08 15:08:30 -0400

    Version 9 Upload
    Updated Pico and MATLAB programs. This should be ready for the final. Just need to tweak windage and elevation adjustments.

--- LaptopSide.m
+++ LaptopSide.m
@@ -2,8 +2,9 @@
 clear s;
 serial_list = serialportlist;
 port = serial_list(length(serial_list)-1);
-%port = 'COM17';
+port = 'COM10';
 s = serialport(port, 9600, 'timeout', 600);
+flush(s);
 
 % Camera setup
 close all;
@@ -12,11 +13,24 @@ x_res = 1920;
 y_res = 1080;
 scalingfactor = 0.15;  % Used to lower the resolution of the camera output
 res = sprintf(('%dx%d'), x_res, y_res);
-cam.Resolution=res;
+%cam.Resolution=res;
 
 % Constant Variables
-minArea = round((x_res/100*scalingfactor)^2); % Minimum area threshold
+%minArea = round((x_res/100*scalingfactor)^2); % Minimum area threshold
 min_circularity = 0.7; % Disgards target if less than this margin
+color_desired = "r";
+number_of_targets = 2;
+
+% Variables Updated in Program Loop
+rel_pos_x = 0;
+rel_pos_y = 0;
+current_target_number = 1;
+error = 0;
+elevation_error = 999;
+azimuth_error = 999;
+pico_shot = 0;
+
+%______________________________________________________________
 
 % Centering the turret
 centering_window = figure();
@@ -30,10 +44,16 @@ function preventDefault(obj,property)
   set(obj,property,@(~,~)0);
 end
 % Centering turret loop
+flush(s);
 while finish == 0
+  pause(0.1);
   % Receive data from pico telling us it is ready for a new instruction
   pico_output = s.readline();
-  flush(s);
+  split_output = split(pico_output.strip(), ',');
+  color_desired = split_output(1,1);
+  minArea = str2num(split_output(2,1));
+  maxArea = str2num(split_output(3,1));
+  %flush(s);
   % Get data from camera
   source_image = snapshot(cam); % grab 1 image
   lowres_image = imresize(source_image, scalingfactor);  % Lowers the resolution for increased performance
@@ -73,129 +93,114 @@ end
 while 1    
     % Receive data from pico telling us it is ready for a new instruction
     pico_output = s.readline();
+    pico_output = str2num(pico_output.strip());
+    
+    if pico_output == 2
+        pico_shot = 1;
+    else
+        pico_shot = 0;
+    end
 
     % Get data from camera
     source_image = snapshot(cam); % grab 1 image
     source_image = imresize(source_image, scalingfactor);  % Lowers the resolution for increased performance
     
-    %______RED_TARGETS______
-    % Apply color threshold
-    [red_thresholded_image, red_masked_image] = redLABMask(source_image); 
-    % Extract properties for all objects
-    red_props = regionprops(red_thresholded_image, 'Centroid', 'Area', 'BoundingBox', 'Circularity');
-    
-    %______YELLOW_TARGETS______
-    % Apply color threshold
-    [yellow_thresholded_image, yellow_masked_image] = yellowLABMask(source_image); 
-    % Extract properties for all objects
-    yellow_props = regionprops(yellow_thresholded_image, 'Centroid', 'Area', 'BoundingBox', 'Circularity');
+    if color_desired == "r"
+        % Apply color threshold
+        [thresholded_image, masked_image] = redLABMask(source_image); 
+    elseif color_desired == "y"
+        % Apply color threshold
+        [thresholded_image, masked_image] = yellowLABMask(source_image); 
+    elseif color_desired == "o"
+        % Apply color threshold
+        [thresholded_image, masked_image] = orangeLABMask(source_image);
+    end
 
-    %______ORANGE_TARGETS______
-    % Apply color threshold
-    [orange_thresholded_image, orange_masked_image] = orangeLABMask(source_image); 
     % Extract properties for all objects
-    orange_props = regionprops(orange_thresholded_image, 'Centroid', 'Area', 'BoundingBox', 'Circularity');
-    
-    % Add Together All Masked Images
-    masked_image = red_masked_image + yellow_masked_image + orange_masked_image;
-    
+    props = regionprops(thresholded_image, 'Centroid', 'Area', 'BoundingBox', 'Circularity');
+
     % Filter out small objects (noise)
-    filtered_red_props = red_props([red_props.Area] > minArea);
-    filtered_yellow_props = yellow_props([yellow_props.Area] > minArea);
-    filtered_orange_props = orange_props([orange_props.Area] > minArea);
+    filtered_props = props([props.Area] > minArea);
+    filtered_props = filtered_props([filtered_props.Area] < maxArea);
 
     % Draw annotated image to the screen for live preview
     %imshowpair(source_image, masked_image, 'montage');  % Use for increased readability
     imagesc(masked_image);  % Use for increased perfomance over imshowpair
-    axis off;
-    hold on;
+    axis off; hold on;
     
-    % Annotate Red Props
-    for k = 1:length(filtered_red_props)
-        rectangle('Position', filtered_red_props(k).BoundingBox, 'EdgeColor', 'r', 'LineWidth', 2);
+    % Annotate Props
+    for k = 1:length(filtered_props)
+        rectangle('Position', filtered_props(k).BoundingBox, 'EdgeColor', 'r', 'LineWidth', 2);
     end
-    % Annotate Yellow Props
-    for k = 1:length(filtered_yellow_props)
-        rectangle('Position', filtered_yellow_props(k).BoundingBox, 'EdgeColor', 'y', 'LineWidth', 2);
-    end
-    % Annotate Orange Props
-    for k = 1:length(filtered_orange_props)
-        rectangle('Position', filtered_orange_props(k).BoundingBox, 'EdgeColor', 'm', 'LineWidth', 2);
-    end
-    title('Live Preview');
-    hold off;
+    title('Live Preview'); hold off;
     
-    % Label the image (RED PROPS)
-    for k = 1:length(filtered_red_props)
-        box = filtered_red_props(k).BoundingBox; % Finds the centroid
+    % Label the image
+    for k = 1:length(filtered_props)
+        box = filtered_props(k).BoundingBox; % Finds the centroid
         centroid = [(box(1)+box(3))/2, (box(2)+box(4))/2]; % Finds the centroid
-        circularity = filtered_red_props(k).Circularity; % Finds the circularity
+        circularity = filtered_props(k).Circularity; % Finds the circularity
         if circularity > min_circularity
             color = 'g';
         else
             color = 'r';
         end
-        text(filtered_red_props(k).Centroid(1)-5, filtered_red_props(k).Centroid(2), sprintf('%.2f', circularity), 'Color', color); % Prints circularity value at centroid
-    end
-    % Label the image (YELLOW PROPS)
-    for k = 1:length(filtered_yellow_props)
-        box = filtered_yellow_props(k).BoundingBox; % Finds the centroid
-        centroid = [(box(1)+box(3))/2, (box(2)+box(4))/2]; % Finds the centroid
-        circularity = filtered_yellow_props(k).Circularity; % Finds the circularity
-        if circularity > min_circularity
-            color = 'g';
-        else
-            color = 'r';
-        end
-        text(filtered_yellow_props(k).Centroid(1)-5, filtered_yellow_props(k).Centroid(2), sprintf('%.2f', circularity), 'Color', color); % Prints circularity value at centroid
-    end
-    % Label the image (ORANGE PROPS)
-    for k = 1:length(filtered_orange_props)
-        box = filtered_orange_props(k).BoundingBox; % Finds the centroid
-        centroid = [(box(1)+box(3))/2, (box(2)+box(4))/2]; % Finds the centroid
-        circularity = filtered_orange_props(k).Circularity; % Finds the circularity
-        if circularity > min_circularity
-            color = 'g';
-        else
-            color = 'r';
-        end
-        text(filtered_orange_props(k).Centroid(1)-5, filtered_orange_props(k).Centroid(2), sprintf('%.2f', circularity), 'Color', color); % Prints circularity value at centroid
+        text(filtered_props(k).Centroid(1)-5, filtered_props(k).Centroid(2), sprintf('%.2f', circularity), 'Color', color); % Prints circularity value at centroid
     end
     
+    % Prints crosshair
     text(x_res*scalingfactor/2-1, y_res*scalingfactor/2-1, '.', 'Color', 'c'); % Prints crosshair
 
-    % Filter out noise
-    target_red_props = filtered_red_props([filtered_red_props.Circularity] > min_circularity);
-    target_yellow_props = filtered_yellow_props([filtered_yellow_props.Circularity] > min_circularity);
-    target_orange_props = filtered_orange_props([filtered_orange_props.Circularity] > min_circularity);
-    red_target_printout = sprintf(('Red Targets: %d'), length(target_red_props));
-    yellow_target_printout = sprintf(('Yellow Targets: %d'), length(target_yellow_props));
-    orange_target_printout = sprintf(('Orange Targets: %d'), length(target_orange_props));
-    text(2, 3, red_target_printout, 'Color', 'w'); % Prints legend
-    text(2, 8, yellow_target_printout, 'Color', 'w'); % Prints legend
-    text(2, 13, orange_target_printout, 'Color', 'w'); % Prints legend
+    % Filter out everything but circles
+    target_props = filtered_props([filtered_props.Circularity] > min_circularity);
+    target_printout = sprintf(('Number of Targets: %d'), length(target_props));
+    text(2, 3, target_printout, 'Color', 'w'); % Prints legend
 
     % Choose a waypoint
-    % "Right now, this just chooses the biggest target on the screen."
-    try
-        red_props_areas = [filtered_red_props.Area];
-        [area, idx] = max(red_props_areas);
-        centroid = [NaN,NaN];
-        centroids = [filtered_red_props.Centroid];
-        centroid = [centroids(idx*2-1),centroids(idx*2)];
-        center = [centroid(1) - (x_res*scalingfactor/2), centroid(2) - (y_res*scalingfactor/2)]
+    if length(target_props) ~= number_of_targets
+        sprintf(('ERROR: %d targets detected'), length(target_props))
+        elevation_error = 0;
+        azimuth_error = 0;
+        error = 1;  % Sets error flag to true
+    else
+        centroids = [filtered_props.Centroid];
+        centroid_1 = [centroids(1),centroids(2)];  % Target 1
+        centroid_2 = [centroids(3),centroids(4)];  % Target 2
+        
+        if rel_pos_x == 0 & rel_pos_y == 0  % If relative position not generated
+            rel_pos_x = centroid_2(1) - centroid_1(1);
+            %rel_pos_y = centroid_2(2) - centroid_1(2);
+        end
+        
+        % Sets the leftmost target as the first to be shot
+        if rel_pos_x > 0
+            targets = [centroid_1; centroid_2];
+        else
+            targets = [centroid_2; centroid_1];
+        end
+        
+        % Selects the centroid corresponding to the target we want
+        current_centroid = targets(current_target_number, :);
+
         % Determine elevation and azimuth errors
-        elevation_error = -int32((centroid(2))-y_res*scalingfactor/2);
-        azimuth_error = int32((centroid(1))-x_res*scalingfactor/2);
-    catch
-         elevation_error = 0;
-         azimuth_error = 0;
-         idx
-         centroids
-     end
+        elevation_error = -int32((current_centroid(2))-y_res*scalingfactor/2);
+        azimuth_error = int32((current_centroid(1))-x_res*scalingfactor/2);
+        [elevation_error, azimuth_error]
+        error = 0;  % Sets error flag to false
+    end
 
     % Send elevation and azimuth errors to pico
     msg_to_pico = sprintf(('%d,%d'), elevation_error, azimuth_error);
     s.writeline(msg_to_pico);
+    
+    % Check if Pico shot
+    if pico_shot == 1
+        current_target_number = current_target_number + 1;
+    end
+    
+    % Check to see if all targets shot
+    if current_target_number > number_of_targets
+        sprintf(('Program Complete'))
+        break;
+    end
     
 end
--- PicoSide.py
+++ PicoSide.py
@@ -5,14 +5,16 @@ import ttyacm
 tty = ttyacm.open(1)
 
 ## Credits
-print("\nRP2040 EW309 Turret Base Program\nVersion: WIP 7\nAuthored by Aaron Goff (͡°͜ʖ°)\n")
+print("\nRP2040 EW309 Turret Base Program\nVersion: WIP 7\n(͡°͜ʖ°)\n")
 
 # Define all functions
 
 def generateDutyCycles(elev_error, azi_error, time_delta, sum_of_error_x, sum_of_error_y):
     # This uses proportional control
-    P = 0.003  # Proportional gain
-    I = 0.0001  # Integral gain
+    #P = 0.003  # Proportional gain
+    #I = 0.0001  # Integral gain
+    P = 0.002;
+    I = 0.00005;
     
     # Elevation Motor
     elev_gain = P * elev_error + I * sum_of_error_y
@@ -27,15 +29,13 @@ def generateDutyCycles(elev_error, azi_error, time_delta, sum_of_error_x, sum_of
     return elev_gain, azi_gain
 
 
-def centerTurret(tty):
+def centerTurret(tty, color, min_target_area, max_target_area):
     centering_speed = 0.2
-    print("Start MATLAB GUI and UVC Batch File Now\n")
-    input("Enter a character when MATLAB and UVC services are started: ")
     print("\nCENTERING TURRET VIA MATLAB\n")
     flag = 0
     while flag != 1:
         # Tell laptop that the pico is ready for a serial command
-        tty.print('1')
+        tty.print(f"{color},{min_target_area},{max_target_area}")
         # Receive command from laptop:
         command_from_laptop = tty.readline()
         command_from_laptop = command_from_laptop.split(",")
@@ -60,27 +60,9 @@ def centerTurret(tty):
     return
 
 def specifyTargets():
-    red = int(input("Any RED targets? (0/1): "))
-    yellow = int(input("Any YELLOW targets? (0/1): "))
-    orange = int(input("Any ORANGE targets? (0/1): "))
-    colors = [red,yellow,orange]
-    number_of_targets = int(input("Total number of targets: "))
-    distance_to_board = int(input("What POSITION is the turret in? (1/2/3): "))
-    print("Assuming targets are all of the MEDIUM size.")
-    
-    target_printout = ""
-    if red == 1:
-        target_printout = target_printout + "red "
-    if yellow == 1:
-        target_printout = target_printout + "yellow "
-    if orange == 1:
-        target_printout = target_printout + "orange "
-        
-    print("____________________________________________________\n")
-    print(f"Using corrections for position {distance_to_board}, the turret will engage {number_of_targets} target(s) of the following color(s): {target_printout}")
-    print("____________________________________________________\n")
-    
-    return colors, number_of_targets, distance_to_board
+    color = input("Color? (r/y/o): ")
+    distance_to_board = int(input("What POSITION is the turret in? (1/2/3): ")) 
+    return color, distance_to_board
 
 def actuateMotors(elev_float, azi_float):
     elev_deadzone = 7500  # Deadzone with a 500 duty cycle margin
@@ -94,63 +76,63 @@ def actuateMotors(elev_float, azi_float):
     if elev_float < 0:
         pitch_motor_down.duty_u16(0)
         pitch_motor_up.duty_u16(elev_duty)
-        print(f"Elev. Output: {int(elev_duty/65535*100)}%\n")
+        #print(f"Elev. Output: {int(elev_duty/65535*100)}%\n")
     else:
         pitch_motor_up.duty_u16(0)
         pitch_motor_down.duty_u16(elev_duty)
-        print(f"Elev. Output: {-int(elev_duty/65535*100)}%\n")
+        #print(f"Elev. Output: {-int(elev_duty/65535*100)}%\n")
     if azi_float > 0:
         yaw_motor_ccw.duty_u16(0)
         yaw_motor_cw.duty_u16(azi_duty)
-        print(f"Azi. Output: {int(azi_duty/65535*100)}%\n")
+        #print(f"Azi. Output: {int(azi_duty/65535*100)}%\n")
     else:
         yaw_motor_cw.duty_u16(0)
         yaw_motor_ccw.duty_u16(azi_duty)
-        print(f"Azi. Output: {-int(azi_duty/65535*100)}%\n")
+        #print(f"Azi. Output: {-int(azi_duty/65535*100)}%\n")
     return
 
 def pulsed_proportional(elev_error, azi_error):
-    pulse_magnitude_x = 0.65
+    pulse_magnitude_x = 0.5
     pulse_magnitude_y = 0.5
-    pulse_duration = 0.012
+    pulse_duration = 0.02
     pulse_duty_x = int(pulse_magnitude_x*65535)
     pulse_duty_y = int(pulse_magnitude_y*65535)
     if elev_error == 0:
         pitch_motor_down.duty_u16(65535)
         pitch_motor_up.duty_u16(65535)
-    elif elev_error > 0:
+    elif elev_error < 0:
         pitch_motor_down.duty_u16(0)
         pitch_motor_up.duty_u16(pulse_duty_y)
-        print("Pulsing Y")
+        #print("Pulsing Y")
     else:
         pitch_motor_up.duty_u16(0)
         pitch_motor_down.duty_u16(pulse_duty_y)
-        print("Pulsing Y")
+        #print("Pulsing Y")
     if azi_error == 0:
         yaw_motor_ccw.duty_u16(65535)
         yaw_motor_cw.duty_u16(65535)
     elif azi_error > 0:
         yaw_motor_ccw.duty_u16(0)
         yaw_motor_cw.duty_u16(pulse_duty_x)
-        print("Pulsing X")
+        #print("Pulsing X")
     else:
         yaw_motor_cw.duty_u16(0)
         yaw_motor_ccw.duty_u16(pulse_duty_x)
-        print("Pulsing X")
+        #print("Pulsing X")
     sleep(pulse_duration)
     # Brake the motors
     yaw_motor_cw.duty_u16(65535)
     yaw_motor_ccw.duty_u16(65535)
     pitch_motor_up.duty_u16(65535)
     pitch_motor_down.duty_u16(65535)
-    sleep(pulse_duration*6)
+    sleep(pulse_duration*4)
     return
 
 def shoot(shots_desired):
     # Initial Variables
     rounds_fired = 0
     sensor_reset = True
-    base_current = 1800
+    base_current = 2500
     # Current Sensor Definitions
     INA260_ADDRESS = 0x40
     INA260_CURRENT_REGISTER = 0x01
@@ -162,10 +144,6 @@ def shoot(shots_desired):
     pitch_motor_up.duty_u16(65535)
     pitch_motor_down.duty_u16(65535)
 
-    # Let motors spool up
-    roller_motors.duty_u16(65535)
-    sleep(3)
-
     while rounds_fired < shots_desired:
         belt_motor.duty_u16(65535)
         data = ina260_i2c.readfrom_mem(INA260_ADDRESS, INA260_CURRENT_REGISTER, 2)
@@ -176,13 +154,12 @@ def shoot(shots_desired):
         if current_ma > base_current and sensor_reset:
             rounds_fired = rounds_fired + 1
             sensor_reset = False
-            print(current_ma)
         if current_ma < base_current:
             sensor_reset = True
-        sleep(0.05)
+        #print(current_ma)
+        sleep(0.025)
     
     # Disable the Motors
-    roller_motors.duty_u16(0)
     belt_motor.duty_u16(0)
     return
 
@@ -211,33 +188,65 @@ belt_motor.freq(500)
 belt_motor.duty_u16(0)
 
 # Ask the user for target details
-colors, number_of_targets, distance_to_board = specifyTargets()
+color, distance_to_board = specifyTargets()
+
+# Change the holdover and number of shots based on distance to target
+if distance_to_board == 1:
+    drop_compensation = 19
+    windage_compensation = 11
+    min_target_area = 80
+    max_target_area = 200
+    shots_desired = 2
+if distance_to_board == 2:
+    drop_compensation = 33
+    windage_compensation = 12
+    min_target_area = 180
+    max_target_area = 450
+    shots_desired = 2
+if distance_to_board == 3:
+    drop_compensation = 50
+    windage_compensation = 16
+    min_target_area = 320
+    max_target_area = 800
+    shots_desired = 3
+
+# Data to Collect Before Starting MATLAB
+print("Start MATLAB GUI and UVC Batch File Now\n")
+input("Enter a character when MATLAB and UVC services are started: ")
 
 # Center the turret
-centerTurret(tty)
+centerTurret(tty, color, min_target_area, max_target_area)
 
 time_delta = 0
 sum_of_error_x = 0
 sum_of_error_y = 0
 pause_movement = False
 targets_engaged = 0
+shot = 0
 
-while targets_engaged < number_of_targets:
+# Let motors spool up
+roller_motors.duty_u16(65535)
+sleep(3)
+
+program_time_start = ticks_ms() # Start collecting time
+
+while targets_engaged < 2:
     start_time = ticks_ms() # Start collecting time
     
     pause_movement = False  # Reset pause variable
     
     # Tell laptop that the pico is ready for a serial command
-    tty.print('1')
-    
-    drop_compensation = 15
-    windage_compensation = 7
+    if not shot:
+        tty.print('1')
+    else:
+        tty.print('2')
+
     # Receive command from laptop:
     command_from_laptop = tty.readline()
     command_from_laptop = command_from_laptop.split(",")
-    print(command_from_laptop)
     elevation_error = int(command_from_laptop[0]) + drop_compensation
     azimuth_error = int(command_from_laptop[1]) + windage_compensation
+    #print(elevation_error, azimuth_error)
     
     if (elevation_error - drop_compensation) == (azimuth_error - windage_compensation) == 0:
         pause_movement = True  # Stop the turret if no targets are seen, but keep sum of errors
@@ -255,7 +264,7 @@ while targets_engaged < number_of_targets:
     elev_float, azi_float = generateDutyCycles(elevation_error, azimuth_error, time_delta, sum_of_error_x, sum_of_error_y)
     # Actuate the motors
     margin_of_error = 15  # Pixels
-    margin_lower_bound = 2 # Pixels
+    margin_lower_bound = 5 # Pixels
     if not pause_movement:
         if abs(azimuth_error) > margin_of_error or abs(elevation_error) > margin_of_error:
             actuateMotors(elev_float, azi_float)
@@ -279,15 +288,39 @@ while targets_engaged < number_of_targets:
         command_from_laptop = command_from_laptop.split(",")
         elevation_error = int(command_from_laptop[0]) + drop_compensation
         azimuth_error = int(command_from_laptop[1]) + windage_compensation
-        print(command_from_laptop)
         
         if abs(elevation_error) < margin_lower_bound and abs(azimuth_error) < margin_lower_bound:
-            shoot(2)
+            shoot(shots_desired)
             targets_engaged = targets_engaged + 1
-            sleep(5)
+            sleep(0.5)
+            shot = 1
+            if targets_engaged == 1:
+                shot_1_x_error = azimuth_error
+                shot_1_y_error = elevation_error  # Saves error for shot
+            else:
+                shot_2_x_error = azimuth_error
+                shot_2_y_error = elevation_error  # Saves error for shot
+        else:
+            shot = 0
         
     # Print report
     time_delta = ticks_ms() - start_time # End collecting time
-    print(f"Frames/Sec: {int(1000/time_delta)}\n")
+    #print(f"Frames/Sec: {int(1000/time_delta)}\n")
 
-print("\n\nProgram Terminated")
+# Disable the Motors
+roller_motors.duty_u16(0)
+
+program_time_end = ticks_ms() # End collecting time
+total_time = (program_time_end - program_time_start)/1000
+
+degrees_per_pixel = 69 / (1920 * 0.15);
+shot_1_x = shot_1_x_error * degrees_per_pixel;
+shot_1_y = shot_1_y_error * degrees_per_pixel;
+shot_2_x = shot_2_x_error * degrees_per_pixel;
+shot_2_y = shot_2_y_error * degrees_per_pixel;
+
+print("\n\n------------- Stats ------------\n\n")
+print(f"Time of Engagement = {total_time} Seconds")
+print(f"Shot 1 Error = {shot_1_x, shot_1_y} Degrees")
+print(f"Shot 2 Error = {shot_2_x, shot_2_y} Degrees")
+print("\n\n------ Program Terminated ------")
